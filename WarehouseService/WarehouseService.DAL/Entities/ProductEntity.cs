083b02ffd32be9ba92bd9e14549040e547b47a97
Daniil Feoktistov <whatdoessence@gmail.com>
2023-01-20 01:46:42 +0300

    Add entities

---
+++ WarehouseService/WarehouseService.DAL/Entities/ProductEntity.cs
@@ -0,0 +1,16 @@
+﻿using WarehouseService.Domain.Entities.Base;
+using WarehouseService.Domain.Enums;
+
+namespace WarehouseService.Domain.Entities
+{
+    public class ProductEntity : EntityBase
+    {
+        public required string Name { get; set; }
+        public required string Description { get; set; }
+        public int Quantity { get; set; }
+        public StockStatus StockStatus { get; set; }
+
+        public int CategoryId { get; set; }
+        public CategoryEntity? Category { get; set; }
+    }
+}
