d0b2081fa3ed7312818b52a316fd1c8e0fda0ce1
Daniil Feoktistov <whatdoessence@gmail.com>
2023-01-20 01:53:00 +0300

    Add contracts

---
+++ WarehouseService/WarehouseService.Application/Contracts/Repositories/IGenericRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq.Expressions;
+using WarehouseService.Domain.Entities.Base;
+
+namespace WarehouseService.Application.Contracts.Repositories
+{
+    public interface IGenericRepository<TEntity> where TEntity : EntityBase
+    {
+        Task<TEntity> Insert(TEntity entity, CancellationToken cancellationToken);
+        Task<IEnumerable<TEntity>> Get(CancellationToken cancellationToken);
+        Task<TEntity> Update(TEntity entity, CancellationToken cancellationToken);
+        Task<TEntity> Delete(int id, CancellationToken cancellationToken);
+        Task<IEnumerable<TEntity>> GetByPredicate(Expression<Func<TEntity, bool>> predicate, CancellationToken cancellationToken);
+    }
+}
